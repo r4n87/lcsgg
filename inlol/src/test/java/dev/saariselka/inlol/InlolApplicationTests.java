50a93913d9e267966c1c27036c683506729b56e6
Yuran Jang <raneythings@gmail.com>
2022-02-27 01:03:10 +0900

    first register

---
+++ inlol/src/test/java/dev/saariselka/inlol/InlolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saariselka.inlol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
