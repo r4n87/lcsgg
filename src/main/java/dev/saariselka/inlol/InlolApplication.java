b09e6736c2c81ad2c5eff6a25876c6abf46051c3
Raney Jang <rxxxyland@gmail.com>
2022-03-14 17:44:48 +0900

    add search page

---
+++ src/main/java/dev/saariselka/inlol/InlolApplication.java
@@ -0,0 +1,13 @@
+package dev.saariselka.inlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InlolApplication.class, args);
+    }
+
+}
