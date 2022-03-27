ebb7ff072400175fa3cf4cf744ee249a06d1b417
JIHYUN <sjh8911@naver.com>
2022-03-27 20:15:12 +0900

    riot_summoner 관련 클래스 추가

--- inlol/src/main/java/dev/saariselka/inlol/controller/SearchController.java
+++ inlol/src/main/java/dev/saariselka/inlol/controller/SearchController.java
@@ -1,5 +1,6 @@
-package dev.saariselka.inlol;
+package dev.saariselka.inlol.controller;
 
+import dev.saariselka.inlol.Summoner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
--- inlol/src/main/java/dev/saariselka/inlol/repository/APIRepository.java
+++ inlol/src/main/java/dev/saariselka/inlol/repository/APIRepository.java
@@ -13,6 +13,6 @@ public interface APIRepository extends JpaRepository<APIEntity, Long> {
 
     public List<APIEntity> findById(String id);
 
-    @Query(value = "SELECT URL FROM MST_API WHERE CATEGORY = :categroy AND OPERATION = :operation", nativeQuery = true)
-    List<APIEntity> searchAPI(@Param("categroy") String category, @Param("operation") String operation);
+    @Query(value = "SELECT URL FROM MST_API WHERE CATEGORY = :category AND OPERATION = :operation", nativeQuery = true)
+    List<APIEntity> searchAPI(@Param("category") String category, @Param("operation") String operation);
 }
