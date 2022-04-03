94a64ca872bad94a8ec2e1f05348c78bed7ebcf4
Yuran Jang <rxxxy@gmail.com>
2022-04-04 01:41:44 +0900

    add MatchParticipant files and constants json files

---
+++ inlol/src/main/java/dev/saariselka/inlol/repository/MatchParticipantRepository.java
@@ -0,0 +1,14 @@
+package dev.saariselka.inlol.repository;
+
+import dev.saariselka.inlol.entity.MatchParticipantEntity;
+import dev.saariselka.inlol.entity.MatchParticipantId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MatchParticipantRepository extends JpaRepository<MatchParticipantEntity, MatchParticipantId> {
+    List<MatchParticipantEntity> findByMatchParticipantId(MatchParticipantId matchParticipantId);
+    List<MatchParticipantEntity> findAllByMatchParticipantId(MatchParticipantId matchParticipantId);
+}
