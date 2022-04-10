8d6ff2d3780cbd62ae0c42accc44053cc871ba9e
massonekim <mayastrok@gmail.com>
2022-04-10 16:59:39 +0900

    1. TeamDto Assembly Class 생성 2. Team Entity 검색 기능 추가

--- inlol/src/main/java/dev/saariselka/inlol/repository/TeamRepository.java
+++ inlol/src/main/java/dev/saariselka/inlol/repository/TeamRepository.java
@@ -5,6 +5,8 @@ import dev.saariselka.inlol.entity.MatchParticipantId;
 import dev.saariselka.inlol.entity.TeamEntity;
 import dev.saariselka.inlol.entity.TeamId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ import java.util.List;
 @Repository
 public interface TeamRepository extends JpaRepository<TeamEntity, TeamId> {
     List<TeamEntity> findByTeamId(TeamId teamId);
+
+    @Query(value = "SELECT * FROM riot_match_team WHERE match_id = :matchId", nativeQuery = true)
+    List<TeamEntity> searchTeamsByMatchId(@Param("matchId") String matchId);
 }
