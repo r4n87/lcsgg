c83d144d89ae6cb00a0a1bb585cbf56dfa65ed04
Raney Jang <rxxxyland@gmail.com>
2022-04-25 23:08:18 +0900

    add inlol_front package

---
+++ inlol_front/src/main/java/dev/saariselka/inlol_front/InlolFrontApplication.java
@@ -0,0 +1,13 @@
+package dev.saariselka.inlol_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlolFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InlolFrontApplication.class, args);
+    }
+
+}
---
+++ inlol_front/src/test/java/dev/saariselka/inlol_front/InlolFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saariselka.inlol_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlolFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
