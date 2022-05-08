607a247751566ab87193557b1547840b2670cbac
JIHYUN <sjh8911@naver.com>
2022-05-08 17:36:47 +0900

    riot_league_miniseries 추가 + LeagueMiniSeriesController + LeagueMiniSeriesEntity + LeagueMiniSeriesId + LeagueMiniSeriesRepository + LeagueMiniSeriesService

---
+++ inlol/src/main/java/dev/saariselka/inlol/entity/LeagueMiniSeriesId.java
@@ -0,0 +1,20 @@
+package dev.saariselka.inlol.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class LeagueMiniSeriesId implements Serializable {
+    @Column(nullable = false)
+    private String summonerId;
+    @Column(nullable = false)
+    private String queueType;
+}
---
+++ inlol/src/main/java/dev/saariselka/inlol/service/LeagueMiniSeriesService.java
@@ -0,0 +1,33 @@
+package dev.saariselka.inlol.service;
+
+import dev.saariselka.inlol.entity.LeagueMiniSeriesEntity;
+import dev.saariselka.inlol.entity.LeagueMiniSeriesId;
+import dev.saariselka.inlol.repository.LeagueMiniSeriesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LeagueMiniSeriesService {
+    @Autowired
+    private final LeagueMiniSeriesRepository leagueMiniSeriesRepository;
+
+    public List<LeagueMiniSeriesEntity> findAll(){
+        List<LeagueMiniSeriesEntity> miniSeriesEntities = new ArrayList<>();
+        leagueMiniSeriesRepository.findAll().forEach(e -> miniSeriesEntities.add(e));
+        return miniSeriesEntities;
+    }
+
+    public List<LeagueMiniSeriesEntity> findByLeagueMiniSeriesId(LeagueMiniSeriesId leagueMiniSeriesId) {
+        return leagueMiniSeriesRepository.findByLeagueMiniSeriesId(leagueMiniSeriesId);
+    }
+
+    public void insert(String summonerId, String queueType, int losses, String progress, int target, int wins, Timestamp rrt) {
+        leagueMiniSeriesRepository.save(new LeagueMiniSeriesEntity(new LeagueMiniSeriesId(summonerId, queueType), losses, progress, target, wins, rrt));
+    }
+}
