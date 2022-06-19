e82ce9430f3e2d04f88e68ccf2c2d8443654a1d6
massonekim <mayastrok@gmail.com>
2022-06-19 19:04:31 +0900

    1. Lombok  Getter Setter 추가에 따른 get,set 함수 제거

--- inlol/src/main/java/dev/saariselka/inlol/dto/BanDto.java
+++ inlol/src/main/java/dev/saariselka/inlol/dto/BanDto.java
@@ -1,7 +1,11 @@
 package dev.saariselka.inlol.dto;
 
 import dev.saariselka.inlol.entity.MatchBanEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class BanDto {
     private String championId;
     private String pickTurn;
@@ -9,21 +13,5 @@ public class BanDto {
     public BanDto(MatchBanEntity matchBanEntity) {
         this.championId = String.valueOf(matchBanEntity.getChampionId());
         this.pickTurn = String.valueOf(matchBanEntity.getMatchBanId().getPickTurn());
-    }
-
-    public String getChampionId() {
-        return championId;
-    }
-
-    public void setChampionId(String championId) {
-        this.championId = championId;
-    }
-
-    public String getPickTurn() {
-        return pickTurn;
-    }
-
-    public void setPickTurn(String pickTurn) {
-        this.pickTurn = pickTurn;
     }
 }
--- inlol/src/main/java/dev/saariselka/inlol/dto/LeagueMiniSeriesDto.java
+++ inlol/src/main/java/dev/saariselka/inlol/dto/LeagueMiniSeriesDto.java
@@ -3,9 +3,13 @@ package dev.saariselka.inlol.dto;
 import dev.saariselka.inlol.entity.LeagueEntryEntity;
 import dev.saariselka.inlol.entity.LeagueMiniSeriesEntity;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class LeagueMiniSeriesDto {
     private String summonerId;
     private String queueType;
@@ -23,51 +27,5 @@ public class LeagueMiniSeriesDto {
         this.losses = leagueMiniSeriesEntity.getLosses();
         this.target = leagueMiniSeriesEntity.getTarget();
         this.progress = leagueMiniSeriesEntity.getProgress().toCharArray();
-    }
-
-    public String getSummonerId() {
-        return summonerId;
-    }
-
-    public void setSummonerId(String summonerId) {
-        this.summonerId = summonerId;
-    }
-
-    public String getQueueType() {
-        return queueType;
-    }
-
-    public void setQueueType(String queueType) {
-        this.queueType = queueType;
-    }
-
-    public int getWins() {
-        return wins;
-    }
-
-    public void setWins(int wins) {
-        this.wins = wins;
-    }
-
-    public int getLosses() {
-        return losses;
-    }
-
-    public void setLosses(int losses) {
-        this.losses = losses;
-    }
-
-    public int getTarget() { return target; }
-
-    public void setTarget(int target) {
-        this.target = target;
-    }
-
-    public char[] getProgress() {
-        return progress;
-    }
-
-    public void setProgress(char[] progress) {
-        this.progress = progress;
     }
 }
