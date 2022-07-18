47b733e3c91a776becaf65e255459c7d855e96de
JIHYUN <sjh8911@naver.com>
2022-07-18 22:18:57 +0900

    DBFacadeTest 작성 - public void setSummonerInfo(HashMap<String, String> result) - public void setLeagueInfo(HashSet<Object> leagueInfos)

--- inlol/src/test/java/dev/saariselka/inlol/facade/DBFacadeTest.java
+++ inlol/src/test/java/dev/saariselka/inlol/facade/DBFacadeTest.java
@@ -1,10 +1,8 @@
 package dev.saariselka.inlol.facade;
 
-import dev.saariselka.inlol.controller.SummonerController;
-import dev.saariselka.inlol.controller.SummonerSpellController;
+import dev.saariselka.inlol.controller.*;
 import dev.saariselka.inlol.dto.SummonerDto;
-import dev.saariselka.inlol.entity.SummonerEntity;
-import dev.saariselka.inlol.entity.SummonerSpellEntity;
+import dev.saariselka.inlol.entity.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.transaction.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +27,12 @@ public class DBFacadeTest {
     SummonerSpellController summonerSpellController;
     @Autowired
     SummonerController summonerController;
+    @Autowired
+    ChampionController championController;
+    @Autowired
+    LeagueEntryController leagueEntryController;
+    @Autowired
+    LeagueMiniSeriesController leagueMiniSeriesController;
 
     @Test
     @DisplayName("Find SummonerDto By SummonerName When SummonerName is Null")
@@ -175,5 +181,255 @@ public class DBFacadeTest {
         //then
         assertThat(summonerSpellImageBarrierFind).isEqualTo("SummonerBarrier.png");
         assertThat(summonerSpellImageBoosterFind).isEqualTo("SummonerBoost.png");
+    }
+
+    @Test
+    @DisplayName("Save Champions")
+    public void setChampions() {
+        // given
+        List<ChampionEntity> entities = new ArrayList<>();
+        ChampionEntity entity1 = new ChampionEntity(999, "champion1", "챔피언1", "Test1.png");
+        ChampionEntity entity2 = new ChampionEntity(998, "champion2", "챔피언2", "Test2.png");
+        entities.add(entity1);
+        entities.add(entity2);
+
+        // when
+        dbFacade.setChampions(entities);
+        String findKo1 = championController.getNameKoByNameEng("champion1");
+        String findImg1 = championController.getImagePathByNameEng("champion1");
+        String findKo2 = championController.getNameKoByNameEng("champion2");
+        String findImg2 = championController.getImagePathByNameEng("champion2");
+
+        // then
+        assertThat(findKo1).isEqualTo("챔피언1");
+        assertThat(findImg1).isEqualTo("Test1.png");
+        assertThat(findKo2).isEqualTo("챔피언2");
+        assertThat(findImg2).isEqualTo("Test2.png");
+    }
+
+    @Test
+    @DisplayName("Save SummonerInfo")
+    public void setSummonerInfo() {
+        // given
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        map.put("id", "TestId");
+        map.put("accountId", "TestAccountId");
+        map.put("puuid", "TestPuuid");
+        map.put("name", "TestUser");
+        map.put("profileIconId", "9999");
+        map.put("revisionDate", "888888888");
+        map.put("summonerLevel", "999");
+
+        // when
+        dbFacade.setSummonerInfo(map);
+        SummonerEntity entity = summonerController.getSummoner("TestPuuid").get(0);
+
+        // then
+        assertThat(entity.getId()).isEqualTo("TestId");
+        assertThat(entity.getAccountid()).isEqualTo("TestAccountId");
+        assertThat(entity.getPuuid()).isEqualTo("TestPuuid");
+        assertThat(entity.getName()).isEqualTo("TestUser");
+        assertThat(entity.getProfileiconid()).isEqualTo(9999);
+        assertThat(entity.getRevisiondate()).isEqualTo(888888888L);
+        assertThat(entity.getSummonerlevel()).isEqualTo(999L);
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When Solo Rank & No MiniSeries")
+    void setLeagueInfoWhenSoloRankAndNoMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWhenNoMiniSeries("RANKED_SOLO_5x5");
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        LeagueEntryEntity find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId").get(0);
+
+        // then
+        assertThat(find.getLeagueEntryId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(find.getLeagueEntryId().getQueueType()).isEqualTo("RANKED_SOLO_5x5");
+        assertThat(find.getLeagueId()).isEqualTo("TestLeagueId");
+        assertThat(find.getSummonerName()).isEqualTo("TestUser");
+        assertThat(find.getTier()).isEqualTo("TestTier");
+        assertThat(find.getRanks()).isEqualTo("II");
+        assertThat(find.getLeaguePoints()).isEqualTo(99);
+        assertThat(find.getWins()).isEqualTo(0);
+        assertThat(find.getLosses()).isEqualTo(0);
+        assertThat(find.isHotStreak()).isTrue();
+        assertThat(find.isVeteran()).isFalse();
+        assertThat(find.isFreshBlood()).isTrue();
+        assertThat(find.isInactive()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When 5x5 Rank & No MiniSeries")
+    void setLeagueInfoWhen55RankAndNoMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWhenNoMiniSeries("RANKED_FLEX_SR");
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        LeagueEntryEntity find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId").get(0);
+
+        // then
+        assertThat(find.getLeagueEntryId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(find.getLeagueEntryId().getQueueType()).isEqualTo("RANKED_FLEX_SR");
+        assertThat(find.getLeagueId()).isEqualTo("TestLeagueId");
+        assertThat(find.getSummonerName()).isEqualTo("TestUser");
+        assertThat(find.getTier()).isEqualTo("TestTier");
+        assertThat(find.getRanks()).isEqualTo("II");
+        assertThat(find.getLeaguePoints()).isEqualTo(99);
+        assertThat(find.getWins()).isEqualTo(0);
+        assertThat(find.getLosses()).isEqualTo(0);
+        assertThat(find.isHotStreak()).isTrue();
+        assertThat(find.isVeteran()).isFalse();
+        assertThat(find.isFreshBlood()).isTrue();
+        assertThat(find.isInactive()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When ETC Rank & No MiniSeries")
+    void setLeagueInfoWhenETCRankAndNoMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWhenNoMiniSeries("TestType");
+
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        List<LeagueEntryEntity> find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId");
+
+        // then
+        assertThat(find.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When Solo Rank & MiniSeries")
+    void setLeagueInfoWhenSoloRankAndMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWithMiniSeries("RANKED_SOLO_5x5");
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        LeagueEntryEntity find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId").get(0);
+        LeagueMiniSeriesEntity miniFind = leagueMiniSeriesController.getLeagueMiniSeriesEntries_ByLeagueMiniSeriesId("TestSummonerId", "RANKED_SOLO_5x5");
+
+        // then
+        assertThat(find.getLeagueEntryId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(find.getLeagueEntryId().getQueueType()).isEqualTo("RANKED_SOLO_5x5");
+        assertThat(find.getLeagueId()).isEqualTo("TestLeagueId");
+        assertThat(find.getSummonerName()).isEqualTo("TestUser");
+        assertThat(find.getTier()).isEqualTo("TestTier");
+        assertThat(find.getRanks()).isEqualTo("I");
+        assertThat(find.getLeaguePoints()).isEqualTo(100);
+        assertThat(find.getWins()).isEqualTo(2);
+        assertThat(find.getLosses()).isEqualTo(1);
+        assertThat(find.isHotStreak()).isTrue();
+        assertThat(find.isVeteran()).isFalse();
+        assertThat(find.isFreshBlood()).isTrue();
+        assertThat(find.isInactive()).isFalse();
+        assertThat(miniFind.getLeagueMiniSeriesId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(miniFind.getLeagueMiniSeriesId().getQueueType()).isEqualTo("RANKED_SOLO_5x5");
+        assertThat(miniFind.getWins()).isEqualTo(2);
+        assertThat(miniFind.getLosses()).isEqualTo(1);
+        assertThat(miniFind.getTarget()).isEqualTo(3);
+        assertThat(miniFind.getProgress()).isEqualTo("ABCDE");
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When 5x5 Rank & MiniSeries")
+    void setLeagueInfoWhen55RankAndMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWithMiniSeries("RANKED_FLEX_SR");
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        LeagueEntryEntity find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId").get(0);
+        LeagueMiniSeriesEntity miniFind = leagueMiniSeriesController.getLeagueMiniSeriesEntries_ByLeagueMiniSeriesId("TestSummonerId", "RANKED_FLEX_SR");
+
+        // then
+        assertThat(find.getLeagueEntryId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(find.getLeagueEntryId().getQueueType()).isEqualTo("RANKED_FLEX_SR");
+        assertThat(find.getLeagueId()).isEqualTo("TestLeagueId");
+        assertThat(find.getSummonerName()).isEqualTo("TestUser");
+        assertThat(find.getTier()).isEqualTo("TestTier");
+        assertThat(find.getRanks()).isEqualTo("I");
+        assertThat(find.getLeaguePoints()).isEqualTo(100);
+        assertThat(find.getWins()).isEqualTo(2);
+        assertThat(find.getLosses()).isEqualTo(1);
+        assertThat(find.isHotStreak()).isTrue();
+        assertThat(find.isVeteran()).isFalse();
+        assertThat(find.isFreshBlood()).isTrue();
+        assertThat(find.isInactive()).isFalse();
+        assertThat(miniFind.getLeagueMiniSeriesId().getSummonerId()).isEqualTo("TestSummonerId");
+        assertThat(miniFind.getLeagueMiniSeriesId().getQueueType()).isEqualTo("RANKED_FLEX_SR");
+        assertThat(miniFind.getWins()).isEqualTo(2);
+        assertThat(miniFind.getLosses()).isEqualTo(1);
+        assertThat(miniFind.getTarget()).isEqualTo(3);
+        assertThat(miniFind.getProgress()).isEqualTo("ABCDE");
+    }
+
+    @Test
+    @DisplayName("Save LeagueInfo When ETC Rank & MiniSeries")
+    void setLeagueInfoWhenETCRankAndMiniSeries() {
+        // given
+        HashSet<Object> leagueInfos = createSummonerInfoWhenNoMiniSeries("TestType");
+
+
+        // when
+        dbFacade.setLeagueInfo(leagueInfos);
+        List<LeagueEntryEntity> find = leagueEntryController.getLeagueEntriesBySummonerId("TestSummonerId");
+        LeagueMiniSeriesEntity miniFind = leagueMiniSeriesController.getLeagueMiniSeriesEntries_ByLeagueMiniSeriesId("TestSummonerId", "TestType");
+
+        // then
+        assertThat(find.isEmpty()).isTrue();
+        assertThat(miniFind).isNull();
+    }
+
+    private HashSet<Object> createSummonerInfoWithMiniSeries(String queueType) {
+        HashSet<Object> leagueInfos = new HashSet<>();
+        LinkedHashMap<String, Object> data = new LinkedHashMap<>();
+        leagueInfos.add(data);
+        data.put("summonerId", "TestSummonerId");
+        data.put("queueType", queueType);
+        data.put("leagueId", "TestLeagueId");
+        data.put("summonerName", "TestUser");
+        data.put("tier", "TestTier");
+        data.put("rank", "I");
+        data.put("leaguePoints", "100");
+        data.put("wins", "2");
+        data.put("losses", "1");
+        data.put("hotStreak", "true");
+        data.put("veteran", "false");
+        data.put("freshBlood", "true");
+        data.put("inactive", "false");
+
+        LinkedHashMap<String, Object> miniSeriesData = new LinkedHashMap<>();
+        data.put("miniSeries", miniSeriesData);
+        miniSeriesData.put("losses", "1");
+        miniSeriesData.put("target", "3");
+        miniSeriesData.put("wins", "2");
+        miniSeriesData.put("progress", "ABCDE");
+
+        return leagueInfos;
+    }
+
+    private HashSet<Object> createSummonerInfoWhenNoMiniSeries(String queueType) {
+        HashSet<Object> leagueInfos = new HashSet<>();
+        LinkedHashMap<String, Object> data = new LinkedHashMap<>();
+        leagueInfos.add(data);
+        data.put("summonerId", "TestSummonerId");
+        data.put("queueType", queueType);
+        data.put("leagueId", "TestLeagueId");
+        data.put("summonerName", "TestUser");
+        data.put("tier", "TestTier");
+        data.put("rank", "II");
+        data.put("leaguePoints", "99");
+        data.put("wins", "0");
+        data.put("losses", "0");
+        data.put("hotStreak", "true");
+        data.put("veteran", "false");
+        data.put("freshBlood", "true");
+        data.put("inactive", "false");
+
+        return leagueInfos;
     }
 }
