5b5b0b65eeae7af2e181739c1ad04fbc9e063f44
Yuran Jang <rxxxy@gmail.com>
2022-08-01 19:27:18 +0900

    API Facade Test 1. getMatchByMatchId 2. getMatchIdListBySummonerPuuidAndMatchStartTime

--- inlol/src/test/java/dev/saariselka/inlol/facade/APIFacadeTest.java
+++ inlol/src/test/java/dev/saariselka/inlol/facade/APIFacadeTest.java
@@ -1,10 +1,15 @@
 package dev.saariselka.inlol.facade;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
@@ -41,5 +46,43 @@ public class APIFacadeTest {
 
         // then
         assertThat(test.get("statusCode")).isEqualTo(200);
+    }
+
+    @Test
+    @DisplayName("[API] Get Match Id List By Summoner Puuid, Match Start Time")
+    void getMatchIdListBySummonerPuuidAndMatchStartTime() {
+        // given
+        String puuid = "UaX2DtUVIdsIuc-chtJJ-hUSgtL32HQPZZhfsqLt6bPzudr80EIbYwAgPQfV8b8eC4sKCkRjx4wrZg";
+        long startTime = 0L;
+
+        // when
+        ArrayList<String> matchIdList = apiFacade.getMatchIdListBySummonerPuuidAndMatchStartTime(puuid, startTime);
+
+        // then
+        assertThat(matchIdList.size()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("[API] Get Match By Match Id")
+    void getMatchByMatchId() throws JsonProcessingException {
+        // given
+        String matchId = "KR_6014432489";
+        ArrayList<String> matchList = new ArrayList<>();
+        matchList.add(matchId);
+
+        // when
+        HashMap<String, Object> testHashMap = apiFacade.getMatchByMatchId(matchList, 0);
+
+        // then
+        assertThat(testHashMap.get("statusCode")).isEqualTo(200);
+
+        HashMap<String, Object> matchInfo = (HashMap<String, Object>) testHashMap.get("body");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonObject testObject =
+                (JsonObject) JsonParser.parseString(mapper.writeValueAsString(matchInfo))
+                                .getAsJsonObject()
+                                .get("metadata");
+
+        assertThat(testObject.get("matchId").getAsString()).isEqualTo(matchId);
     }
 }
