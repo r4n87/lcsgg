56933614cae28d19b4ee489e42bb401f6cf1ef7c
r4n87 <rxxxyland@gmail.com>
2022-10-12 20:27:44 +0900

    Commit lcsgg project

---
+++ lcsgg/src/main/java/dev/saariselka/lcsgg/LcsggApplication.java
@@ -0,0 +1,13 @@
+package dev.saariselka.lcsgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcsggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcsggApplication.class, args);
+    }
+
+}
---
+++ lcsgg/src/test/java/dev/saariselka/lcsgg/LcsggApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saariselka.lcsgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcsggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
