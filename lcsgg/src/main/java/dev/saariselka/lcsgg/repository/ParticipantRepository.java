fe29aa18da9fe5038ea74506e29d8c4942f5b828
r4n87 <rxxxyland@gmail.com>
2022-10-18 23:20:34 +0900

    Fix Repository Extends parameters

--- lcsgg/src/main/java/dev/saariselka/lcsgg/repository/ParticipantRepository.java
+++ lcsgg/src/main/java/dev/saariselka/lcsgg/repository/ParticipantRepository.java
@@ -1,9 +1,9 @@
 package dev.saariselka.lcsgg.repository;
 
-import dev.saariselka.lcsgg.entity.MiniSeries;
+import dev.saariselka.lcsgg.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParticipantRepository extends JpaRepository<MiniSeries, Long> {
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 }
